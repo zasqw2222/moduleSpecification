f7001495c7add804694f01f4c4c6e7a2bb4f9e1f
zasqw2222 <zasqw2222@126.com>
2016-04-26 14:38:20 +0800

    init

---
+++ module.js
@@ -0,0 +1,9 @@
+(function(root,definition){
+	if(typeof define === 'function')
+		define(definition);
+	}else if(typeof module !== 'undefined' && module.exports){
+		module.exports = definition();
+	}else{
+		root[fefinition.fnName] = definition();
+	}
+})(this,function(){});
